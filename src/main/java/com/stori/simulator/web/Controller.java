1a35aa4783adbf6bd0420a698b0bf5523db7010a
daryl-chen <102713030+daryl-chen@users.noreply.github.com>
2025-06-10 21:33:13 +0800

    Update Controller.java

--- src/main/java/com/stori/simulator/web/Controller.java
+++ src/main/java/com/stori/simulator/web/Controller.java
@@ -26,6 +26,8 @@ public class Controller {
         try {
             mqProducerService.sendMessage(msg);
             mqProducerService.sendMessage(msg);
+            mqProducerService.sendMessage(msg);
+            mqProducerService.sendMessage(msg);
         } catch (Exception e) {
             LOGGER.error("发送消息失败");
         }
