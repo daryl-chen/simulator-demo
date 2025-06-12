bb97516be151aa069a4c937b4c9d3ea0f2031e25
daryl-chen <102713030+daryl-chen@users.noreply.github.com>
2025-06-12 21:27:14 +0800

    Update src/main/java/com/stori/simulator/mq/consumer/properties/MqConsumerProperties.java
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- src/main/java/com/stori/simulator/mq/consumer/properties/MqConsumerProperties.java
+++ src/main/java/com/stori/simulator/mq/consumer/properties/MqConsumerProperties.java
@@ -10,7 +10,18 @@ public class MqConsumerProperties {
     private String namesrvAddr;
 
     private String group;
+    private String group;
     private String id;
++ 
+    public String getId() {
+        return id;
+    }
++ 
+    public void setId(String id) {
+        this.id = id;
+    }
++ 
+    public String getNamesrvAddr() {
 
     public String getNamesrvAddr() {
         return namesrvAddr;
